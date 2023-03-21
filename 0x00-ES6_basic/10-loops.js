33730d888eacfb41ca93f39efd222f93146e8f7f
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-03-20 19:33:10 -0700

    Rewrite the function appendToEachArrayValue to use ES6’s for...of operator. And don’t forget that var is not ES6-friendly

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
+  // eslint-disabled-next-line
   for (let value of array) {
+    // eslint-disable-next-line
     value = appendString + value;
   }
 
