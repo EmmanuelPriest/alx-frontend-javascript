166149bbe83505b683b122c37f5b6f9b6ced7ce1
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-03-24 17:26:33 -0700

    Implement a function named initializeRooms that returns an array of 3 ClassRoom objects

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const sizes = [19, 20, 34];
-  const rooms = sizes.map(size => new ClassRoom(size));
-  return rooms;
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
-
-//export default initializeRooms;
