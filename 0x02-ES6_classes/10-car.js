9d63e6c202dafe72509f1aadce5cb44aaf97108d
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-03-24 17:06:09 -0700

    Implement a class named Car

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,24 @@
+export class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    const constructor = Object.getPrototypeOf(this).constructor;
+    return new constructor(this._brand, this._motor, this._color);
+  }
+}
