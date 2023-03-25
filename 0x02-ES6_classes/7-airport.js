96f4d6c88e76d891384403d9949d29da9287dc32
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-03-24 18:19:43 -0700

    Implement a class named Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,32 +1,16 @@
 export default class Airport {
   constructor(name, code) {
-    this._name = name;
-    this._code = code;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
-    }
-    this._name = name;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(code) {
-    if (typeof code !== 'string') {
+    } else if (typeof code !== 'string') {
       throw new TypeError('Code must be a string');
     }
+
+    this._name = name;
     this._code = code;
   }
 
-  toString() {
-    return this._code;
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
   }
 }
